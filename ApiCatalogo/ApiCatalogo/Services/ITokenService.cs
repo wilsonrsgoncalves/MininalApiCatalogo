3ea6862b93e231cac2e4631c1f2a61263c57cd53
Wilson Gonçalves <wilssp@gmail.com>
2025-07-29 18:13:43 -0300

    Implementação minimal api.

---
+++ ApiCatalogo/ApiCatalogo/Services/ITokenService.cs
@@ -0,0 +1,7 @@
+﻿using ApiCatalogo.Models;
+
+namespace ApiCatalogo.Services;
+public interface ITokenService
+{
+    string GerarToken(string key, string issuer,string audience, UserModel user);
+}
